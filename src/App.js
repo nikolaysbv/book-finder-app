d7c4d5640e2c0ae7f93c3bd8b7642273cd65f0d8
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-04-27 22:32:34 +0300

    title and form added, initial css
    Cleaned all unnecessary files and stuff, title and form added, played a bit with their css.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1 className="title">Book Finder App</h1>
+      <section className="form">
+        <form>
+          <h2 className="form__title">Try anything</h2>
+          <input type="text" className="form__input" />
+        </form>
+      </section>
+      <section className="books"></section>
+    </main>
+  )
 }
 
-export default App;
+export default App
